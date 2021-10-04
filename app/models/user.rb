0e1fe854013c5672bf0e0c8c6205c4cb162a31aa
GarrettF <grfoster24@gmail.com>
2021-10-04 16:15:23 -0400

    added controller and routes, got user auth working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :products
+  has_many :posts
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
+  # validates :is_admin, inclusion: { in: [true, false] }
 end
