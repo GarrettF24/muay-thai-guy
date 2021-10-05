307ccdc6a18f747e52b1fe8ac04b2cfead05ada9
GarrettF <grfoster24@gmail.com>
2021-10-04 20:27:15 -0400

    added in some migrations to fix products schema, db is now rendering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_184050) do
+ActiveRecord::Schema.define(version: 2021_10_05_001558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,13 @@ ActiveRecord::Schema.define(version: 2021_10_04_184050) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.string "image_url"
     t.string "review"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "brand"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
