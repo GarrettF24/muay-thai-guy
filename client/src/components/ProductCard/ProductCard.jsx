7aedd1b281699241d16c5deb37a7f9b4af95f9ad
GarrettF <grfoster24@gmail.com>
2021-10-07 11:53:19 -0400

    productdetail responive done, still working on desktop.

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard(props) {
     <div className="product-card">
       <Link to={`/products/${props.product.id}`}>
         <img
-          className="product-image"
+          className="product-card-image"
           src={props.product.image_url}
           alt={props.product.name}
         />
--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { getOneProduct } from "../../services/products"
 import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
+import "./ProductDetail.css"
 // import { useHistory } from "react-router"
 
 export default function ProductDetail(props) {
@@ -30,25 +31,28 @@ export default function ProductDetail(props) {
       <div className="product-image">
         <img src={product.image_url} alt="image" />
       </div>
-      <div className="product-info">
-        <h3>{product.name}</h3>
-        <h3>{product.category}</h3>
-        <h3>{product.brand}</h3>
-      </div>
-      <div className="product-review">
-        <p>{product.review}</p>
-      </div>
-      <div className="product-crud">
-        {props.currentUser !== null ? (
-          <div className="crud-buttons">
-            <Link to={`/products/${id}/edit`}>
-              <button>Edit</button>
-            </Link>
-            <button onClick={() => props.handleProductDelete(product.id)}>
-              Delete
-            </button>
-          </div>
-        ) : null}
+      <div className="product-info-container">
+        <div className="product-info">
+          <h3>Name: {product.name}</h3>
+          <h3>Category: {product.category}</h3>
+          <h3>Brand: {product.brand}</h3>
+        </div>
+        <div className="product-review">
+          <h5>Review...</h5>
+          <p>{product.review}</p>
+        </div>
+        <div className="product-crud">
+          {props.currentUser !== null ? (
+            <div className="crud-buttons">
+              <Link to={`/products/${id}/edit`}>
+                <button>Edit</button>
+              </Link>
+              <button onClick={() => props.handleProductDelete(product.id)}>
+                Delete
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   )
